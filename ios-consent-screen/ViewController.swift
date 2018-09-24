1cead477110b82419a622a81d58ab156dab1ec81
Roger Misteli <roger.misteli@abacus.ch>
2018-09-24 16:13:48 +0200

    Initial Commit

---
+++ ios-consent-screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-consent-screen
+//
+//  Created by Roger Misteli on 24.09.18.
+//  Copyright © 2018 ABACUS Research AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
